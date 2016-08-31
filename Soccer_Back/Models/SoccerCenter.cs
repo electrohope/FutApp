94b64ed27380600f1bbb47abeda4d957206e2863
electrohope <maxicampos@stratosarts.com>
2016-08-30 23:39:49 -0300

    agregacion SoccerCenter y SoccerField

---
+++ Soccer_Back/Models/SoccerCenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer_Back.Models
+{
+    class SoccerCenter
+    {
+        public int SoccerCenterId { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string phone { get; set; }
+        
+        public ICollection<SoccerField> SoccerFields { get; set; }
+    }
+}
---
+++ Soccer_Back/Models/SoccerField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soccer_Back.Models
+{
+    class SoccerField
+    {
+        public int SoccerFieldId { get; set; }
+        public int SoccerCenterId { get; set; }
+        public int SizeField { get; set; }
+        public SoccerCenter SoccerCenter { get; set; }
+    }
+}
